e6e8671e342aa6b14b2a72f060cc2257dc7f4045
vatai <a_Stalker@mail.ru>
2020-10-13 10:19:56 +1100

    Updated composer.json file

---
+++ index.php
@@ -0,0 +1,9 @@
+<?php
+require __DIR__ . '/vendor/autoload.php';
+use Testing\Php\Runner;
+
+
+
+
+
+print_r(Runner\run());
---
+++ src/Runner.php
@@ -0,0 +1,22 @@
+<?php
+namespace Testing\Php\Runner;
+$autoloadPath1 = __DIR__ . '/../../../autoload.php';
+$autoloadPath2 = __DIR__ . '/../vendor/autoload.php';
+
+if (file_exists($autoloadPath1)) {
+    require_once $autoloadPath1;
+} else {
+    require_once $autoloadPath2;
+}
+
+use Tightenco\Support\Collection;
+
+function run()
+{
+    $collection = collect(['taylor', 'abigail', null])->map(function ($name) {
+        return strtoupper($name);
+    });
+
+    return $collection;
+}
+echo run();
